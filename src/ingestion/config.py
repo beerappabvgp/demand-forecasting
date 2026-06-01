41403bd199d214d6c0b1e0f376d0546585c23ca7
beerappabvgp <beerappabharathb@gmail.com>
2026-06-01 20:45:54 +0530

    Prepare dataset registration structure

--- src/ingestion/config.py
+++ src/ingestion/config.py
@@ -4,4 +4,25 @@ from pathlib import Path
 
 @dataclass
 class DataIngestionConfig:
+
     raw_data_dir: Path = Path("data/raw/m5")
+
+    downloads_dir: Path = Path(
+        "data/raw/m5/downloads"
+    )
+
+    sales_dir: Path = Path(
+        "data/raw/m5/sales"
+    )
+
+    calendar_dir: Path = Path(
+        "data/raw/m5/calendar"
+    )
+
+    prices_dir: Path = Path(
+        "data/raw/m5/prices"
+    )
+
+    metadata_dir: Path = Path(
+        "data/raw/m5/metadata"
+    )
